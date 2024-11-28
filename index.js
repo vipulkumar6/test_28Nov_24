9e9226852ac339430c0723d16d618081d171c170
vipul <itsvipulkumar02@gmail.com>
2024-11-29 00:15:45 +0530

    react file created

---
+++ index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const index = () => {
+    return (
+        <div>
+            <div className="box">Name</div>
+            <div className="box">Role</div>
+            <div className="box">Delivery</div>
+            <div className="box">TL</div>
+        </div>
+    )
+}
+
+export default index
